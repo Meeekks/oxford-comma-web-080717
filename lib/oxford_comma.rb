65148642873c2e8da6471cff5e1e9623478c39e9
Kenneth Jiang <k3nyyj@gmail.com>
2017-07-28 19:07:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_ele = array.pop
+    last_ele = ", and " + last_ele
+    array.join(", ") + last_ele
+  end
 end
